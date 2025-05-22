fcadb6bef819298063f9841692a00d4caaaeaf82
fr4ncopiscoya <francomazns@gmail.com>
2025-05-22 10:40:29 -0500

    update(): update template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./pages/dashboard/dashboard.component'),
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard'
+    }
+];
